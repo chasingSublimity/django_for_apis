5e0206fc7fea484052838829b3d0c20165256b5f
Blake Sager <blake.sager@activeprospect.com>
2020-06-03 09:13:37 -0500

    Finish todo backend, start frontend

--- todo/backend/todos/tests.py
+++ todo/backend/todos/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Todo
 
 # Create your tests here.
+class TodoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Todo.objects.create(title='first todo', body='a body here')
+
+    def test_title_content(self):
+        todo = Todo.objects.get(id=1)
+        expected = f'{todo.title}'
+        self.assertEqual(expected, 'first todo')
+    
+    def test_body_content(self):
+        todo = Todo.objects.get(id=1)
+        expected = f'{todo.body}'
+        self.assertEqual(expected, 'a body here')
+    
